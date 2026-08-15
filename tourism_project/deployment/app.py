6aba04907554c51a2778a1a032684fdc5b09045e
Jamshida-em <Jamshi.em@gmail.com>
2026-08-15 16:23:19 +0530

    update tourism_project/deployment/app.py

--- tourism_project/deployment/app.py
+++ tourism_project/deployment/app.py
@@ -7,35 +7,35 @@ import joblib
 model_path = os.path.join(os.path.dirname(__file__), "best_tourism_model_v1.joblib")
 model = joblib.load(model_path)
 
-st.title("Tourism Package Prediction App")
+st.title("Tourism Prediction App")
 st.write("""
-This application predicts predicts whether a customer will purchase the newly introduced Wellness Tourism Package before contacting them.
-Please enter the data below to get a prediction.
+This application predicts the likelihood of a customer accepting tourism package based on their details.
+Enter the data below to get a prediction.
 """)
 
-AgeGroup = st.selectbox("Age Group", ["18-25","26-41","42-57","58-76+"])
-TypeofContact = st.selectbox("Type of Contact", ["Self Enquiry","Company Invited"])
+age    = st.number_input("Age", 18, 75, 30, 1)
+TypeofContact = st.selectbox("Type of Contact", ["Company Invited", "Self Enquiry"])
 CityTier = st.selectbox("City Tier", [1,2,3])
 DurationOfPitch = st.number_input("Duration Of Pitch", 1, 150, 15, 1)
-Occupation = st.selectbox("Occupation", ["Salaried","Small Business","Large Business","Free Lancer"])
+Occupation = st.selectbox("Occupation", ["Free Lancer", "Salaried","Large Business","Small Business"])
 Gender = st.selectbox("Gender", ["Male", "Female"])
 NumberOfPersonVisiting = st.number_input("Number Of Person Visiting", 1, 10, 2, 1)
 NumberOfFollowups = st.number_input("Number Of Follow ups", 1, 10, 2, 1)
-ProductPitched = st.selectbox("Product Pitched", ["Basic","Standard","Deluxe","Super Deluxe","King"])
+ProductPitched = st.selectbox("Product Pitched", ["Basic", "Deluxe","Standard","Super Deluxe","King"])
 PreferredPropertyStar = st.number_input("Preferred Property Star", 1, 5, 3, 1)
-MaritalStatus = st.selectbox("Marital Status", [ "Single","Married","Divorced"])
+MaritalStatus = st.selectbox("Marital Status", ["Married", "Single","Divorced"])
 NumberOfTrips = st.number_input("Number Of Trips", 1, 20, 2, 1)
-Passport = st.selectbox("Passport", [1, 0], format_func=lambda x: "Yes" if x == 1 else "No")
+Passport = st.selectbox("Passport", [0,1])
 PitchSatisfactionScore = st.number_input("Pitch Satisfaction Score", 1, 5, 3, 1)
-OwnCar = st.selectbox("Own Car", [1, 0], format_func=lambda x: "Yes" if x == 1 else "No")
+OwnCar = st.selectbox("Own Car", [0,1])
 NumberOfChildrenVisiting = st.number_input("Number Of Children Visiting", 0, 10, 0, 1)
-Designation = st.selectbox("Designation", ["Manager","Senior Manager","Executive","VP","AVP"])
+Designation = st.selectbox("Designation", ["Executive", "Manager","Senior Manager","VP","AVP"])
 MonthlyIncome = st.number_input("Monthly Income", 100, 100000, 5000, 100)
 
 
 
 input_data = pd.DataFrame([{
-    "AgeGroup" : AgeGroup,
+    "Age" : age,
     "TypeofContact" : TypeofContact,
     "CityTier" : CityTier,
     "DurationOfPitch" : DurationOfPitch,
